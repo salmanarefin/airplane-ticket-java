a00726599a40891991076f0ea2a378333960cabe
salmanarefin <115951122+salmanarefin@users.noreply.github.com>
2022-10-17 01:43:41 +0600

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import classes.*;
+import interfaces.*;
+
+public class Start
+{
+	public static void main(String[] args)
+	{
+		Cover co = new Cover();
+		co.setVisible(true);
+	}
+}
